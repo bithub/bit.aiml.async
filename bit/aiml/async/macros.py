b5da333da8c3aa19e79b36ffb5167a5e7580daee
Ryan Northey <ryan@3ca.org.uk>
2012-06-09 20:29:15 +0100

    componentizing macros

---
+++ bit/aiml/async/macros.py
@@ -0,0 +1,42 @@
+from zope.interface import implements
+from zope.component import getUtility
+
+from bit.core.interfaces import ISockets
+from bit.aiml.async.interfaces import IAIMLMacro
+
+
+class BotAIMacro(object):
+    implements(IAIMLMacro)
+
+    def __init__(self, request):
+        self.request = request
+
+    def _asker(self,jid):
+        # this is very bad! - we should check against my bot's jid domain!                                                                            
+        if 'rooms' in jid:
+            askerid = (jid.split('/')[-1])
+        else:
+            askerid = (jid.split('@')[0])
+        return askerid
+
+    def speak(self,jid,resp,askerid=None,token=None):
+        sockets = getUtility(ISockets)
+        resource = jid.split('/')[1]
+
+        proto = getUtility(ICurateBotProtocol)
+        self.ai.respond_async(jid,resp)
+
+        if 'bot' in sockets.sockets and resource in sockets.sockets['bot']:
+            getUtility(ISockets).emit('bot',resource,'respond',resp,token=token)
+
+        elif 'rooms' in jid:
+            rooms = getUtility(IMUCBot)
+            rooms.speak(jid,resp,askerid)
+
+        else:
+            proto.speak(jid, resp)
+
+
+    def complete(self):
+        pass
+
